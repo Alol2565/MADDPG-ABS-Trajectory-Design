a1db5d5d7b0f29cc7c4274e0e134b491a6193787
Alol <alisaei90@gmail.com>
2022-07-06 15:56:05 +0430

    hyperparams printed

--- test_maddpg_agent.py
+++ test_maddpg_agent.py
@@ -16,8 +16,13 @@ def obs_list_to_state_vector(observation):
 num_users = 100
 num_BSs = 2
 num_uavs = 2
+users_zones=[1,2,3,4]
+users_var=40
+max_user_in_obs=10
+normal_scalar_decay = 0.8
+learning_period = 16
 reward_weights = np.array([10, 10, 0, 0, 1, 0]) / num_users
-env = Environment('Env-1', n_users=num_users, n_uavs=num_uavs, n_BSs=num_BSs, users_zones=[1,2,3,4],users_var=40 ,flight_time=200, max_user_in_obs=10, reward_weights=reward_weights)
+env = Environment('Env-1', n_users=num_users, n_uavs=num_uavs, n_BSs=num_BSs, users_zones=users_zones,users_var=users_var ,flight_time=200, max_user_in_obs=max_user_in_obs, reward_weights=reward_weights)
 
 n_agents = env.num_uavs
 actor_dims = []
@@ -58,6 +63,19 @@ for agent in maddpg_agents.agents:
 stop_learning = False
 learning_rate_decay = True
 
+with open(str(save_dir) + "/specs.txt", "w") as text_file:
+    text_file.write("Number of Users: %s\n" %(num_users))
+    text_file.write("Number of Base Stations: %s\n" %(num_BSs))
+    text_file.write("Number of UAVs: %s\n" %(num_uavs))
+    text_file.write("Reward Weights: %s\n" %(reward_weights))
+    text_file.write("Maximum user in observation: %s\n" %max_user_in_obs)
+    text_file.write("Variance of Users: %s\n" %users_var)
+    text_file.write("Users Zones: %s\n" %users_zones)
+    text_file.write("Normal Scalar: %s\n" %maddpg_agents.agents[0].normal_scalar)
+    text_file.write("Normal Scalar Decay: %s\n" %(normal_scalar_decay))
+    text_file.write("Learning Period: %s\n" %(learning_period))
+
+
 for e in range(episodes):
     obs = env.reset()
     if(maddpg_agents.agents[0].normal_scalar < 0.25):
@@ -68,7 +86,7 @@ for e in range(episodes):
     else:
         if e % 10 == 0:
             for agent in maddpg_agents.agents:
-                agent.normal_scalar *= 0.85
+                agent.normal_scalar *= normal_scalar_decay
         
     done = [False] * n_agents
     score = 0
@@ -78,7 +96,7 @@ for e in range(episodes):
         state = obs_list_to_state_vector(obs)
         state_ = obs_list_to_state_vector(obs_)
         memory.store_transition(obs, state, actions, reward, obs_, state_, done)
-        if maddpg_agents.curr_step % 16 == 0 and not stop_learning:
+        if maddpg_agents.curr_step % learning_period == 0 and not stop_learning:
             maddpg_agents.learn(memory)
         logger.log_step(np.mean(reward), info['num connected users'], info['avg bit rate'])
         obs = obs_
